d8462c64084e31776bd2dee982f0febb3870a563
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-05 23:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ const counterEl = document.getElementById("counter");
+    const button = document.getElementById("incrementBtn");
+
+    button.addEventListener("click", () => {
+      const currentValue = parseInt(counterEl.textContent);
+      alert(`Current value before increment: ${currentValue}`);
+      counterEl.textContent = currentValue + 1;
+    });
